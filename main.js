1f848010320f34843a3e8ab98f05d66150e2ec27
Sofiavaricio <127760001+Sofiavaricio@users.noreply.github.com>
2023-08-16 11:48:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var nome ="Sofia ";
+var sobrenome ="Varicio de Sousa";
+var nomeCompleto = nome.concat (sobrenome);
+console.log(nomeCompleto);
+
+var comprimentoNome = nomeCompleto.length;
+console.log(comprimentoNome);
+
+var irmao = nomeCompleto.replace("Sofia ","Floquinho Paçoca ");
+console.log(irmao);
+
+var upperCase = nomeCompleto.toUpperCase();
+console.log(upperCase);
+
+var lowerCase = nomeCompleto.toLowerCase();
+console.log(lowerCase);
+
+//Matematica
+
+var a = 16;
+var b = 4;
+
+var raizQuadrada = Math.sqrt(a);
+console.log(raizQuadrada);
+
+var maximo = Math.max(a,b);
+console.log(maximo);
+
+var minimo = Math.min(a,b);
+console.log(minimo);
+
+//adicao
+var adicao = a + b;
+console.log(adicao);
+
+// subtracao
+var subtracao = a - b;
+console.log(subtracao);
+//divisao
+var divisao = a / b;
+console.log(divisao);
+//multiplicacao
+var multiplicacao = a * b;
+console.log(multiplicacao);
